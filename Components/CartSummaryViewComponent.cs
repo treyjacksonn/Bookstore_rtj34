95a86017eb1bcc0da149155cade460a4ffbf84e3
Trey Jackson <rtj34@byu.edu>
2023-03-16 14:14:15 -0600

    Cart updated

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore_rtj34.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore_rtj34.Components
+{
+        public class CartSummaryViewComponent : ViewComponent
+        {
+            private Basket cart;
+            public CartSummaryViewComponent(Basket cartService)
+            {
+                cart = cartService;
+            }
+            public IViewComponentResult Invoke()
+            {
+                return View(cart);
+            }
+        }
+}
